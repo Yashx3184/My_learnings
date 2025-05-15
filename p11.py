937fde4d35fc281ccae1cfc5f043ee15208957b0
Yashx3184 <yashwanth1st2005@gmail.com>
2025-05-15 12:46:50 +0530

    p11

--- p11.py
+++ p11.py
@@ -1,2 +1,13 @@
 import numpy as np
-arr = 
+#column_stack()
+
+a1 = np.array([2, 4, 9])
+a2 = np.array([300, 7, 5])
+a3 = np.array([0, 101, 323])
+
+# column_stack() 
+# The elements from the numpy arrays, column wise are taken and a row is created.
+#Note that the number of elements in the i/p arrays must be same.
+#It works on only 1D array           
+print(np.column_stack((a1, a2)))
+print(np.column_stack((a1, a2, a3)))
